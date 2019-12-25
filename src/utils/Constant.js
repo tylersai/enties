9db1468358bf7c6dabd2e990cfe64161be0b9769
Sai Han <thesaihan@gmail.com>
2019-12-25 20:35:05 +0630

    Add toQueryString:func in Constant.js

--- src/utils/Constant.js
+++ src/utils/Constant.js
@@ -1,1 +1,11 @@
+export const API_END_POINT = process.env.API_END_POINT;
 export const API_KEY = process.env.API_KEY;
+
+export const toQueryString = (obj) => {
+
+    const str = [];
+    for(const key in obj){
+        str.push(encodeURIComponent(key.toString()) + '=' + encodeURIComponent(obj[key].toString()));
+    }
+    return str.join('&');
+};
