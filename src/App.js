fcd2e2f18c5aa4153e9e63516a942302b171083e
Sai Han <thesaihan@gmail.com>
2019-12-26 21:54:41 +0630

    Add poster path

--- src/App.js
+++ src/App.js
@@ -1,23 +1,24 @@
-import React, { useState } from 'react';
-import Axio from 'axios';
-import './App.css';
+import React, { useState } from "react";
+import Axio from "axios";
+import "./App.css";
 
-import { API_END_POINT, API_KEY, toQueryString } from './utils/Constant';
+import { API_END_POINT, API_KEY, toQueryString } from "./utils/Constant";
 
-import Navbar from './components/Navbar';
-import Search from './components/Search';
+import Navbar from "./components/Navbar";
+import Search from "./components/Search";
 
 function App() {
-
   const [movieList, setMovieList] = useState([]);
 
-  const processSearch = async (obj) => {
+  const processSearch = async obj => {
+    document.getElementById("search").style.height = "initial";
+
     const queryObj = {
-      api_key : API_KEY
+      api_key: API_KEY
     };
     Object.assign(queryObj, obj);
-    
-    const link = API_END_POINT + '/search/movie?' + toQueryString(queryObj);
+
+    const link = API_END_POINT + "/search/movie?" + toQueryString(queryObj);
     console.log(link);
     const res = await Axio.get(link);
     setMovieList(res.data.results);
@@ -26,22 +27,27 @@ function App() {
   return (
     <div className="App">
       <section id="search">
-        <Navbar/>
-        <br/>
+        <Navbar />
+        <br />
         <h1 className="ent-text-shadow">Let's Watch Movie</h1>
-        <br/>
+        <br />
         <Search process={processSearch} />
-        <br/>
-        {
-          movieList.map(m => (
-            <div key={m.id} className="movie-card">
+        <br />
+      </section>
+      <section id="result">
+        {movieList.map(m => (
+          <div key={m.id} className="movie-card">
+            <img
+              className="poster"
+              src={`https://image.tmdb.org/t/p/w500${m.poster_path}`}
+              alt="Poster"
+            />
+            <div className="description">
               <h3>{m.title}</h3>
               <p>{m.overview}</p>
             </div>
-          ))
-        }
-      </section>
-      <section id="result">
+          </div>
+        ))}
       </section>
     </div>
   );
