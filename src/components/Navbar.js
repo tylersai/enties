a836fada39f1192699ae0f746d9c68accb408b45
Sai Han <thesaihan@gmail.com>
2019-12-27 19:34:52 +0630

    Fix Navbar with proper flex

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,16 +1,20 @@
-import React from 'react';
-import './Navbar.css';
+import React from "react";
+import "./Navbar.css";
 
-const Navbar = (props) => {
-
-    return (
-        <nav className="Navbar">
-            <div id="logo">Enties</div>
-            <div className="nav-menu"><h3>Pricing</h3></div>
-            <div className="nav-menu"><h3>Login</h3></div>
-        </nav>
-    );
-
+const Navbar = props => {
+  return (
+    <nav className="Navbar">
+      <div id="logo">Enties</div>
+      <div id="nav-menu">
+        <div className="nav-menu">
+          <h3>Pricing</h3>
+        </div>
+        <div className="nav-menu">
+          <h3>Login</h3>
+        </div>
+      </div>
+    </nav>
+  );
 };
 
 export default Navbar;
