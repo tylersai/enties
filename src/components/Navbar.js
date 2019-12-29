b6bd467684efa21c481a18c59c277225632877af
Sai Han <thesaihan@gmail.com>
2019-12-29 12:28:47 +0630

    Add ThemeButton component

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,11 +1,13 @@
 import React from "react";
 import "./Navbar.css";
+import ThemeButton from "./ThemeButton";
 
 const Navbar = props => {
   return (
     <nav className="Navbar">
       <div id="logo">Enties</div>
       <div id="nav-menu">
+        <ThemeButton className="nav-menu"/>
         <div className="nav-menu">
           <h3>Pricing</h3>
         </div>
---
+++ src/components/ThemeButton.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import './ThemeButton.css';
+
+const ThemeButton = (props) => {
+
+    return (
+        <div className="ThemeButton">
+            <label className="switch switch_type2" role="switch" aria-checked>
+          <input type="checkbox" className="switch__toggle"/>
+          <span className="switch__label"></span>
+        </label>
+        </div>
+    );
+
+}
+
+export default ThemeButton;
