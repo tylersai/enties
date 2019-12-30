9da0ba0826f0c41d60bfa0a116e0b00b2c0b9256
Sai Han <thesaihan@gmail.com>
2019-12-30 22:47:52 +0630

    Movie card

--- src/components/ResultSection.js
+++ src/components/ResultSection.js
@@ -1,24 +1,33 @@
 import React from "react";
 import "./ResultSection.css";
+import movie from "../assets/movie-dark.svg";
 
-import { POSTER_PATH } from '../utils/Constant';
+import { POSTER_PATH } from "../utils/Constant";
 
 const ResultSection = ({ movieList }) => {
   return (
-    <section className="ResultSection" id="result">
-      {movieList.map(m => (
-        <div key={m.id} className="movie-card">
-          <img
-            className="poster"
-            src={POSTER_PATH + m.poster_path}
-            alt="Poster"
-          />
-          <div className="description">
-            <h3>{m.title}</h3>
-            <p>{m.overview}</p>
-          </div>
+    <section className="ResultSection bg bg1" id="result">
+      {movieList.length !== 0 ? (
+        <div className="res-wrapper">
+          {movieList.map(m => (
+            <div key={m.id} className="movie-card bg bg2">
+
+              <div className="movie-header">
+                <div className="movie-poster">
+                  <img src={ POSTER_PATH + m.poster_path} alt="POSTER" />
+                </div>
+                <div className="movie-title">
+                  <h4>{m.title}</h4>
+                </div>
+              </div>
+
+              <div className="movie-desc">
+                <span>Description</span><br/>{m.overview}
+              </div>
+            </div>
+          ))}
         </div>
-      ))}
+      ) : null}
     </section>
   );
 };
