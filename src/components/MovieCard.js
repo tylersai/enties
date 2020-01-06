ccf1166144b2058dd6720bbbe89dc18f6a0bfd9c
Sai Han <thesaihan@gmail.com>
2020-01-07 00:08:55 +0630

    Separate MovieCard as a new component

---
+++ src/components/MovieCard.js
@@ -0,0 +1,47 @@
+import React from "react";
+import "./MovieCard.css";
+import movie from "../assets/movie-dark.svg";
+
+import { POSTER_PATH } from "../utils/Constant";
+import Rating from "./Rating";
+
+const MovieCard = ({ m }) => {
+  return (
+    <div className="movie-card bg bg2">
+      <div className="movie-header">
+        <div className="movie-poster">
+          {m.poster_path ? (
+            <img src={POSTER_PATH + m.poster_path} alt="POSTER" />
+          ) : (
+            <img src={movie} alt="POSTER" />
+          )}
+        </div>
+        <div className="movie-title">
+          <h4 className="fg fg1">{m.title}</h4>
+          <div className="fg fg3 year-genre">
+            {m.release_date
+              ? new Date(m.release_date).getFullYear() + " \u2022"
+              : null}{" "}
+            Romance
+          </div>
+          <button className="price-tag bg bg2">$19.99</button>
+          <div className="movie-rating">
+            {m.vote_average && m.vote_count ? (
+              <Rating voteAverage={m.vote_average} voteCount={m.vote_count} />
+            ) : null}
+          </div>
+        </div>
+      </div>
+
+      <div className="movie-desc">
+        <p className="fg fg2">
+          <span>Description</span>
+          <br />
+          {m.overview}
+        </p>
+      </div>
+    </div>
+  );
+};
+
+export default MovieCard;
--- src/components/ResultSection.js
+++ src/components/ResultSection.js
@@ -1,11 +1,9 @@
 import React, { useEffect } from "react";
 import "./ResultSection.css";
-import movie from "../assets/movie-dark.svg";
 import cross from "../assets/cross.svg";
 
-import { POSTER_PATH } from "../utils/Constant";
 import Loading from "./Loading";
-import Rating from "./Rating";
+import MovieCard from "./MovieCard";
 
 const ResultSection = ({
   isLoading,
@@ -41,43 +39,7 @@ const ResultSection = ({
       ) : movieList.length !== 0 ? (
         <div className="res-wrapper">
           {movieList.map(m => (
-            <div key={m.id} className="movie-card bg bg2">
-              <div className="movie-header">
-                <div className="movie-poster">
-                  {m.poster_path ? (
-                    <img src={POSTER_PATH + m.poster_path} alt="POSTER" />
-                  ) : (
-                    <img src={movie} alt="POSTER" />
-                  )}
-                </div>
-                <div className="movie-title">
-                  <h4 className="fg fg1">{m.title}</h4>
-                  <div className="fg fg3 year-genre">
-                    {m.release_date
-                      ? new Date(m.release_date).getFullYear() + " \u2022"
-                      : null}{" "}
-                    Romance
-                  </div>
-                  <button className="price-tag bg bg2">$19.99</button>
-                  <div className="movie-rating">
-                    {m.vote_average && m.vote_count ? (
-                      <Rating
-                        voteAverage={m.vote_average}
-                        voteCount={m.vote_count}
-                      />
-                    ) : null}
-                  </div>
-                </div>
-              </div>
-
-              <div className="movie-desc">
-                <p className="fg fg2">
-                  <span>Description</span>
-                  <br />
-                  {m.overview}
-                </p>
-              </div>
-            </div>
+            <MovieCard key={m.id} m={m}/>
           ))}
         </div>
       ) : (
