6ef8e84dc63a3d38f56ebde17d48b27adf156229
Sai Han <thesaihan@gmail.com>
2020-01-07 16:02:36 +0630

    Successfully add theme change features

--- src/components/ThemeButton.js
+++ src/components/ThemeButton.js
@@ -2,10 +2,29 @@ import React from "react";
 import "./ThemeButton.css";
 
 const ThemeButton = props => {
+
+  const swapStyle = (b,w) => {
+    const rootStyle = getComputedStyle(document.documentElement);
+
+    const black = rootStyle.getPropertyValue(b);
+    const white = rootStyle.getPropertyValue(w);
+
+    document.documentElement.style.setProperty(b, white);
+    document.documentElement.style.setProperty(w, black);
+  }
+
+  const changeTheme = e => {
+    swapStyle('--bokehblack', '--bokehwhite');
+    
+    swapStyle('--entblack', '--entwhite');
+    swapStyle('--entdark', '--entlight');
+    swapStyle('--entdarkgray', '--entlightgray');
+  }
+
   return (
     <div className="ThemeButton">
       <label className="switch switch_type2" role="switch" aria-checked>
-        <input type="checkbox" className="switch__toggle" />
+        <input onChange={changeTheme} type="checkbox" className="switch__toggle" />
         <span className="switch__label" />
       </label>
     </div>
