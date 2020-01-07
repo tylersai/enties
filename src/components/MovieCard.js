d0e257e7c798fbad3c884f230abf546e939b280e
Sai Han <thesaihan@gmail.com>
2020-01-07 17:09:54 +0630

    Handle movie overview being null

--- src/components/MovieCard.js
+++ src/components/MovieCard.js
@@ -6,6 +6,8 @@ import { POSTER_PATH } from "../utils/Constant";
 import Rating from "./Rating";
 
 const MovieCard = ({ m }) => {
+  const options = { year: "numeric", month: "short", day: "numeric" };
+
   return (
     <div className="movie-card bg bg2 ent-shadow">
       <div className="movie-header">
@@ -18,12 +20,13 @@ const MovieCard = ({ m }) => {
         </div>
         <div className="movie-title">
           <h4 className="fg fg1">{m.title}</h4>
-          <div className="fg fg3 year-genre">
-            {m.release_date
-              ? new Date(m.release_date).getFullYear() + " \u2022"
-              : null}{" "}
-            Romance
-          </div>
+
+          {m.release_date ? (
+            <div className="fg fg3 release-date">
+              {new Date(m.release_date).toLocaleDateString("en-US", options)}
+            </div>
+          ) : null}
+
           <button className="price-tag bg bg2">$19.99</button>
           <div className="movie-rating">
             {m.vote_average && m.vote_count ? (
@@ -33,13 +36,15 @@ const MovieCard = ({ m }) => {
         </div>
       </div>
 
-      <div className="movie-desc">
-        <p className="fg fg2">
-          <span>Description</span>
-          <br />
-          {m.overview}
-        </p>
-      </div>
+      {m.overview ? (
+        <div className="movie-desc">
+          <p className="fg fg2">
+            <span>Description</span>
+            <br />
+            {m.overview}
+          </p>
+        </div>
+      ) : null}
     </div>
   );
 };
