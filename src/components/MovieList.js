089383131b5f49c94ae57c9377c3f56e03896246
Sai Han <thesaihan@gmail.com>
2020-01-08 16:05:27 +0630

    Add See All > in discover list

--- src/components/MovieList.js
+++ src/components/MovieList.js
@@ -2,16 +2,18 @@ import React from "react";
 import { Link } from "react-router-dom";
 
 import "./MovieList.css";
+import right from "../assets/right.svg";
 import MovieCardSmall from "./MovieCardSmall";
 
 const MovieList = ({ icon, title, list }) => {
   return (
-    <div className="MovieList list discover-list">
+    <div className={"MovieList list" + (title === "Discover" ? " discover-list":"")}>
       <div className="list-header bg bg3">
         <img src={icon} alt="D" />
         <h2>{title}</h2>
         <Link to="/search?q=love">
-            See All &gt;
+          <h4>See All</h4>
+          <img src={right} alt=">"/>
         </Link>
       </div>
       <div className="list-body bg bg2 fg fg3">
