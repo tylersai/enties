1fce5323f2c37d1eb879bd80776be93b29033235
Sai Han <thesaihan@gmail.com>
2020-01-12 17:53:46 +0630

    Add build v1.1

---
+++ build/precache-manifest.7b1baea2112bc9a08455076b83bed59a.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "61e93ba275d52bb5f2ec8a13cd6135b3",
+    "url": "/index.html"
+  },
+  {
+    "revision": "96c4beb14a1ec8430a55",
+    "url": "/static/css/main.9196040b.chunk.css"
+  },
+  {
+    "revision": "1a8f6990376e223ed841",
+    "url": "/static/js/2.2106db6e.chunk.js"
+  },
+  {
+    "revision": "84a2a7a390df1ac99fa4fadf23155e60",
+    "url": "/static/js/2.2106db6e.chunk.js.LICENSE"
+  },
+  {
+    "revision": "96c4beb14a1ec8430a55",
+    "url": "/static/js/main.eb42c88c.chunk.js"
+  },
+  {
+    "revision": "71e0b32779825ab86970",
+    "url": "/static/js/runtime-main.b25a4863.js"
+  },
+  {
+    "revision": "4ac9ab46a0801fcc31f67ef69fd879db",
+    "url": "/static/media/cross.4ac9ab46.svg"
+  },
+  {
+    "revision": "806964950292d90ea39bbe17eb2d991a",
+    "url": "/static/media/discover.80696495.svg"
+  },
+  {
+    "revision": "a9bcd107a0aaa8a7bc3aaae8834663ba",
+    "url": "/static/media/movie-dark.a9bcd107.svg"
+  },
+  {
+    "revision": "f4e0186916480ad37a456f293cf98a1e",
+    "url": "/static/media/right.f4e01869.svg"
+  },
+  {
+    "revision": "dccccb006550c762297066ffe8ac4939",
+    "url": "/static/media/star-empty.dccccb00.svg"
+  },
+  {
+    "revision": "407ac3fdf31bc100180fd409725e03ed",
+    "url": "/static/media/star-full.407ac3fd.svg"
+  },
+  {
+    "revision": "0b062b6a3bcb356543cf12431c850c34",
+    "url": "/static/media/star-half.0b062b6a.svg"
+  },
+  {
+    "revision": "28590520e9164def0851d17b81abf365",
+    "url": "/static/media/trend.28590520.svg"
+  }
+]);
