13245c960d9c347dafae04426466256a6131e048
Sai Han <thesaihan@gmail.com>
2020-01-24 23:32:40 +0630

    Handle missing poster in MovieCardSmall

--- src/components/element/MovieCardSmall.js
+++ src/components/element/MovieCardSmall.js
@@ -1,6 +1,7 @@
 import React from "react";
 import { useHistory } from "react-router-dom";
 import "./MovieCardSmall.css";
+import movie from "../../assets/movie-dark.svg";
 
 import { POSTER_PATH } from "../../utils/Constant";
 
@@ -11,9 +12,17 @@ const MovieCardSmall = ({ m }) => {
 
   return (
     <div onClick={goDetail} className="MovieCardSmall animate-enlarge">
-      <img src={POSTER_PATH + m.poster_path} alt="POSTER"/>
-      <div>
-          <h5 className="fg fg3">{m.title}</h5>
+      <div className="img-section">
+      {
+        m.poster_path ? (
+            <img className="with-poster" src={POSTER_PATH + m.poster_path} alt="POSTER" />
+          ) : (
+            <img className="no-poster" src={movie} alt="POSTER" />
+          )
+      }
+      </div>
+      <div className="caption-section">
+        <h5 className="fg fg3">{m.title}</h5>
       </div>
     </div>
   );
