cee227c53769983fa5d067a2a2520da7059027ec
Sai Han <thesaihan@gmail.com>
2020-01-24 22:02:50 +0630

    Remove data fetching in RelatedMovies.js

--- src/components/element/RelatedMovies.js
+++ src/components/element/RelatedMovies.js
@@ -1,36 +1,17 @@
-import React, { useState, useEffect } from "react";
-import Axios from "axios";
+import React from "react";
 import "./RelatedMovies.css";
 
-import { API_END_POINT, API_KEY } from "../../utils/Constant";
 import MovieCardSmall from "./MovieCardSmall";
 import Loading from "../ui/Loading";
 
-const RelatedMovies = ({type, movie_id}) => {
-  const [loadRelated, setLoadRelated] = useState(false);
-  const [relatedMovies, setRelatedMovies] = useState([]);
-
-  useEffect(() => {
-    setLoadRelated(true);
-    Axios.get(API_END_POINT + `/movie/${movie_id}/${type}?api_key=${API_KEY}`)
-      .then(res => {
-        setLoadRelated(false);
-        setRelatedMovies(res.data.results);
-      })
-      .catch(err => {
-        console.log(err);
-        setLoadRelated(false);
-        setRelatedMovies([]);
-      });
-  }, [type, movie_id]);
-
+const RelatedMovies = ({ loadRelated, relatedMovies }) => {
   return (
     <div className="RelatedMovies">
-        {
-          loadRelated ? <Loading/>:relatedMovies.map(
-                movie => <MovieCardSmall key={movie.id} m={movie}/>
-            )
-        }
+      {loadRelated ? (
+        <Loading />
+      ) : (
+        relatedMovies.map(movie => <MovieCardSmall key={movie.id} m={movie} />)
+      )}
     </div>
   );
 };
