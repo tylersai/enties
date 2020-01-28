fe80a8da55baa2236d921eb37a971d55c2b7125f
Sai Han <thesaihan@gmail.com>
2020-01-29 00:10:26 +0630

    Add Label in CollectionParts

--- src/components/element/CollectionParts.js
+++ src/components/element/CollectionParts.js
@@ -3,12 +3,15 @@ import "./CollectionParts.css";
 import MovieCard from "./MovieCard";
 
 const CollectionParts = ({ parts }) => {
-
   return (
     <div className="CollectionParts">
-      {parts.map(m => (
-        <MovieCard m={m} />
-      ))}
+      <h3 className="fg fg3">Movies in This Bundle</h3>
+      <hr align="left" className="fg fullwidth" />
+      <div className="wrapper">
+        {parts.map(m => (
+          <MovieCard m={m} />
+        ))}
+      </div>
     </div>
   );
 };
