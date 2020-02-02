9b9c1dd4e3a3125341914f78805f2a95d9a7107e
Sai Han <thesaihan@gmail.com>
2020-02-02 22:54:52 +0630

    Add ActorPage.js

---
+++ src/components/page/ActorPage.js
@@ -0,0 +1,8 @@
+import React, { useState, useEffect } from "react";
+import "./ActorPage.css";
+
+const ActorPage = ({ match }) => {
+  return <section className="ActorPage bg bg1 animate-popup" />;
+};
+
+export default ActorPage;
