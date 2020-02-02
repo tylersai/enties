b31d18ee548f02fec4e03a1eed265ad237782e34
Sai Han <thesaihan@gmail.com>
2020-02-02 23:03:57 +0630

    Add basic page requirements

--- src/components/page/ActorPage.js
+++ src/components/page/ActorPage.js
@@ -1,8 +1,30 @@
 import React, { useState, useEffect } from "react";
 import "./ActorPage.css";
+import Axio from "axios";
+
+import actorLogo from "../../assets/actor.svg";
+import { API_END_POINT, API_KEY, POSTER_PATH } from "../../utils/Constant";
+
+import Loading from "../ui/Loading";
+import NoData from "../element/NoData";
 
 const ActorPage = ({ match }) => {
-  return <section className="ActorPage bg bg1 animate-popup" />;
+  const [isLoading, setIsLoading] = useState(false);
+  const [actor, setActor] = useState({});
+
+  return (
+    <section className="ActorPage bg bg1 animate-popup">
+      {isLoading ? (
+        <div className="center-loading">
+          <Loading />
+        </div>
+      ) : actor.name ? (
+        <div />
+      ) : (
+        <NoData svgPath={actorLogo} label="ACTOR NOT FOUND" />
+      )}
+    </section>
+  );
 };
 
 export default ActorPage;
