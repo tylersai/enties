692c0608786e86fb381228a2a0dbfb19af5e0212
Sai Han <thesaihan@gmail.com>
2020-02-04 14:02:37 +0630

    Prepare KeywordsBlock reuse in SearchPage

--- src/components/element/KeywordsBlock.js
+++ src/components/element/KeywordsBlock.js
@@ -6,26 +6,38 @@ import { API_END_POINT, API_KEY } from "../../utils/Constant";
 
 import Keyword from "../ui/Keyword";
 
-const KeywordsBlock = ({ movie_id }) => {
+const KeywordsBlock = ({ movie_id, searchQuery }) => {
   const [keywords, setKeywords] = useState([]);
   useEffect(() => {
-    Axios.get(API_END_POINT + `/movie/${movie_id}/keywords?api_key=${API_KEY}`)
+    let apiLink =
+      API_END_POINT +
+      (searchQuery ? `/search/keyword` : `/movie/${movie_id}/keywords`);
+    apiLink += `?api_key=${API_KEY}`;
+    if (searchQuery) apiLink += `&query=${searchQuery}`;
+    Axios.get(apiLink)
       .then(res => {
-        setKeywords(res.data.keywords);
+        setKeywords(res.data[searchQuery ? "results" : "keywords"]);
       })
       .catch(err => {
         console.log(err);
         setKeywords([]);
       });
-  }, [movie_id]);
+  }, [movie_id, searchQuery]);
 
-  if(keywords.length > 0)
-  return (
-    <div>
-      <div className="fg fg3 section-label">Keywords : </div>
-      {keywords.map(kw => <Keyword key={kw.id} keyword={kw}/>)}
-    </div>
-  );
+  if (keywords.length > 0)
+    return (
+      <div className="KeywordsBlock">
+        {searchQuery ? (
+          <h2 className="fg fg2">Related Keywords</h2>
+        ) : (
+          <div className="fg fg3 section-label">Keywords : </div>
+        )}
+
+        {keywords.map(kw => (
+          <Keyword key={kw.id} keyword={kw} />
+        ))}
+      </div>
+    );
   else return null;
 };
 
