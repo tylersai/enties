2b55456e7a49944aaf92e3ec0aac62a0d9d24a26
Sai Han <thesaihan@gmail.com>
2020-02-07 00:18:38 +0630

    Layout with FoldedBox

--- src/components/ui/FoldedBox.js
+++ src/components/ui/FoldedBox.js
@@ -4,6 +4,15 @@ import "./FoldedBox.css";
 const FoldedBox = ({title, totalResults, children}) => {
     return(
         <div className="FoldedBox">
+            <div className="header-bar ent-text-shadow">
+                <div className="type-title">
+                    <h2>{title}</h2>
+                </div>
+                <div className="total-results fg fgg">{totalResults}</div>
+                <div className="fold-btn">
+                    <button></button>
+                </div>
+            </div>
             {children}
         </div>
     );
