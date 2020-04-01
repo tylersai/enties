0b573a98da42b0110d2577b72f667f5ae5eb0eef
Sai Han <thesaihan@gmail.com>
2020-04-01 22:40:55 +0630

    Implement Avatar component in ReviewBlock

--- src/components/element/ReviewsBlock.js
+++ src/components/element/ReviewsBlock.js
@@ -2,6 +2,7 @@ import React, { useEffect, useState } from "react";
 import "./ReviewsBlock.css";
 import Axios from "axios";
 import { API_END_POINT, API_KEY } from "../../utils/Constant";
+import Avatar from "../ui/Avatar";
 
 const ReviewsBlock = ({ movie_id }) => {
   const [reviews, setReviews] = useState([]);
@@ -30,7 +31,10 @@ const ReviewsBlock = ({ movie_id }) => {
         <div className="review-wrapper">
           {reviews.slice(0, 10).map(r => (
             <div className="review fg fg3" key={r.id}>
-              <h4>{r.author}</h4>
+              <div className="review-header">
+                <Avatar name={r.author} />
+                <div className="review-header-author"><h4>{r.author}</h4></div>
+              </div>
               <p className="ent-small-text">{r.content}</p>
             </div>
           ))}
