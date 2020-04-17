aec25c4cf87ba4176534cd07d02bf2db4eb4140a
Sai Han <thesaihan@gmail.com>
2020-04-17 15:31:04 +0630

    Add SearchBar component

--- src/components/ui/SearchBar.js
+++ src/components/ui/SearchBar.js
@@ -1,24 +1,22 @@
-import React, { useState } from "react";
+import React from "react";
 import "./SearchBar.css";
 
 const SearchBar = () => {
 
+  const ref = React.createRef();
+
   const onSearch = e => {
     e.preventDefault();
-    alert("Searched!");
-  };
-
-  const expand = () => {
-    document.getElementById("search-btn").classList.toggle("close");
-    document.getElementById("search-input").classList.toggle("square");
+    console.log(ref.current.value);
   };
 
   return (
     <div className="SearchBar bg bg2">
-      <form id="content" onSubmit={onSearch}>
-        <input type="text" name="input" className="input" id="search-input" />
-        <button type="reset" className="search" id="search-btn" onClick={expand}></button>
-      </form>
+      <div className="search-bar-wrapper">
+        <form onSubmit={onSearch}>
+          <input type="search" name="query" className="fg fg-primary" ref={ref} placeholder="Search" />
+        </form>
+      </div>
     </div>
   );
 }
