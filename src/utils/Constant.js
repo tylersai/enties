9a23fe9ce74da76126deb6996e36a8b8f65ae3a7
Sai Han <thesaihan@gmail.com>
2020-05-03 22:02:29 +0630

    Remove const API_KEY

--- src/utils/Constant.js
+++ src/utils/Constant.js
@@ -1,12 +1,11 @@
 export const API_END_POINT = process.env.REACT_APP_API_END_POINT;
-export const API_KEY = process.env.REACT_APP_API_KEY;
 export const POSTER_PATH = process.env.REACT_APP_POSTER_PATH;
 export const POSTER_PATH_FULL = process.env.REACT_APP_POSTER_PATH_FULL;
 
 export const toQueryString = (obj) => {
 
     const str = [];
-    for(const key in obj){
+    for (const key in obj) {
         str.push(encodeURIComponent(key.toString()) + '=' + encodeURIComponent(obj[key].toString()));
     }
     return str.join('&');
