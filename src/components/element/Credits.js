b9b70dfa7063086769b287cbc663432d165ec983
Sai Han <thesaihan@gmail.com>
2021-05-23 14:22:56 +0630

    Casts and crews display and more link

--- src/components/element/Credits.js
+++ src/components/element/Credits.js
@@ -6,28 +6,35 @@ import { API_END_POINT } from "../../utils/Constant";
 import ActorLink from "../ui/ActorLink";
 
 const Credits = ({ movie_id }) => {
-
   const [casts, setCasts] = useState([]);
   const [crews, setCrews] = useState([]);
+  const [castsDisplay, setCastsDisplay] = useState([]);
+  const [crewsDisplay, setCrewsDisplay] = useState([]);
 
   useEffect(() => {
     Axios.get(API_END_POINT + `/movie/${movie_id}/credits`)
-      .then(res => {
-        setCasts(res.data.cast);
-        setCrews(removeDuplicate(res.data.crew));
+      .then((res) => {
+        const casts = res.data.cast;
+        const crews = removeDuplicate(res.data.crew);
+        setCasts(casts);
+        setCrews(crews);
+        setCastsDisplay(casts.slice(0, 20));
+        setCrewsDisplay(crews.slice(0, 20));
       })
-      .catch(err => {
+      .catch((err) => {
         console.log(err);
         setCasts([]);
         setCrews([]);
+        setCastsDisplay([]);
+        setCrewsDisplay([]);
       });
   }, [movie_id]);
 
-  const removeDuplicate = arr => {
+  const removeDuplicate = (arr) => {
     let res = [];
     let ids = [];
     if (arr && arr.length > 0) {
-      arr.forEach(a => {
+      arr.forEach((a) => {
         if (ids.indexOf(a.id) < 0) {
           ids.push(a.id);
           res.push(a);
@@ -37,34 +44,77 @@ const Credits = ({ movie_id }) => {
     return res;
   };
 
-  if (casts.length === 0 && crews.length === 0)
-    return null;
+  const moreCasts = (e) => {
+    e.preventDefault();
+    setCastsDisplay([
+      ...castsDisplay,
+      ...casts.slice(castsDisplay.length, castsDisplay.length + 10),
+    ]);
+  };
+
+  const moreCrews = (e) => {
+    e.preventDefault();
+    setCrewsDisplay([
+      ...crewsDisplay,
+      ...crews.slice(crewsDisplay.length, crewsDisplay.length + 10),
+    ]);
+  };
+
+  if (casts.length === 0 && crews.length === 0) return null;
 
   return (
     <div className="Credits">
-      <h3 className="fg fg3 text-center" style={{ marginTop: "7vh" }}>Cast &amp; Crew</h3>
+      <h3 className="fg fg3 text-center" style={{ marginTop: "7vh" }}>
+        Cast &amp; Crew
+      </h3>
       <hr className="fg" />
       <div className="credit-wrapper">
-        {casts.length > 0 ? (<div className="credit-col">
-          <div className="left">
-            <h4 className="fg fgg">Actors</h4>
+        {casts.length > 0 ? (
+          <div className="credit-col">
+            <div className="left">
+              <h4 className="fg fgg">Actors</h4>
+            </div>
+            <div className="right">
+              {castsDisplay.map((c) => (
+                <ActorLink aid={c.id} key={c.id + c.credit_id}>
+                  {c.name}
+                </ActorLink>
+              ))}
+              {casts.length > 20 && castsDisplay.length !== casts.length && (
+                <a
+                  href="/"
+                  className="fg fg-primary ent-small-text more-link"
+                  onClick={moreCasts}
+                >
+                  + More
+                </a>
+              )}
+            </div>
           </div>
-          <div className="right">
-            {
-              casts.map(c => <ActorLink aid={c.id} key={c.id + c.credit_id}>{c.name}</ActorLink>)
-            }
+        ) : null}
+        {crews.length > 0 ? (
+          <div className="credit-col">
+            <div className="left">
+              <h4 className="fg fgg">Crews</h4>
+            </div>
+            <div className="right" id="crew-right">
+              {crewsDisplay.map((c) => (
+                <ActorLink aid={c.id} key={c.id + c.credit_id}>
+                  {c.name}
+                </ActorLink>
+              ))}
+              {crews.length > 20 && crewsDisplay.length !== crews.length && (
+                <a
+                  href="/"
+                  className="fg fg-primary ent-small-text more-link"
+                  onClick={moreCrews}
+                >
+                  + More
+                </a>
+              )}
+            </div>
           </div>
-        </div>) : null}
-        {crews.length > 0 ? (<div className="credit-col">
-          <div className="left">
-            <h4 className="fg fgg">Crews</h4>
-          </div>
-          <div className="right">
-            {
-              crews.map(c => <ActorLink aid={c.id} key={c.id + c.credit_id}>{c.name}</ActorLink>)
-            }
-          </div>
-        </div>) : null}
+        ) : null}
       </div>
     </div>
   );
