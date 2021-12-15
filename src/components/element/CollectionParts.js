4e5657ae52d8da69a1186b5d15d2ce3dadb37cd2
Sai Han <thesaihan@gmail.com>
2021-12-15 16:10:47 +0630

    change wrapper: list of movie cards in Collection Page with grid

--- src/components/element/CollectionParts.js
+++ src/components/element/CollectionParts.js
@@ -7,8 +7,8 @@ const CollectionParts = ({ parts }) => {
     <div className="CollectionParts">
       <h3 className="fg fg3">Movies in This Bundle</h3>
       <hr align="left" className="fg fullwidth" />
-      <div className="wrapper">
-        {parts.map(m => (
+      <div className="res-wrapper" style={{ padding: "3vmin" }}>
+        {parts.map((m) => (
           <MovieCard m={m} key={m.id} />
         ))}
       </div>
